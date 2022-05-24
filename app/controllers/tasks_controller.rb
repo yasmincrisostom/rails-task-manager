3ca6dfa4e7d4492c153658d632a7dc7da64f6db3
yasmin crisostomo <crisostomyasmin@gmail.com>
2022-05-24 15:28:16 -0300

    Refactoring

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
   end
 
   def new
@@ -19,18 +21,18 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
     @task.update(task_params)
 
     redirect_to task_path(@task)
   end
 
   def destroy
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
     @task.destroy
 
     # no need for app/views/restaurants/destroy.html.erb
@@ -41,5 +43,9 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'tasks', to: 'tasks#index'
-  get 'tasks/new', to: 'tasks#new', as: :new_task # The `new` route needs to be *before* `show` route.
-  get 'tasks/:id', to: 'tasks#show', as: :task
-  post 'tasks', to: 'tasks#create'
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-  patch 'tasks/:id', to: 'tasks#update'
-  delete 'tasks/:id', to: 'tasks#destroy'
+  # get 'tasks', to: 'tasks#index'
+  # get 'tasks/new', to: 'tasks#new', as: :new_task # The `new` route needs to be *before* `show` route.
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # post 'tasks', to: 'tasks#create'
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete 'tasks/:id', to: 'tasks#destroy'
+  resources :tasks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
